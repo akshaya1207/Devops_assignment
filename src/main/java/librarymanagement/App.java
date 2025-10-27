4c62c3f550bee9d8927d9a5368365f80d056b7f5
akshaya1207 <akshaya12072004@gmail.com>
2025-10-27 09:47:10 +0530

    first commit

---
+++ src/main/java/librarymanagement/App.java
@@ -0,0 +1,11 @@
+package librarymanagement;
+
+public class App {
+
+	
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to the Library Management System!");
+	    }
+	
+
+}
